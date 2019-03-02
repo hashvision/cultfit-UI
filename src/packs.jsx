5d9950069b4ace64f5c59e23b73305c68d40429c
Raj Sagar <therajsagar@gmail.com>
2019-03-03 00:54:06 +0530

    updates

--- src/packs.jsx
+++ src/packs.jsx
@@ -19,6 +19,92 @@ const Packs =() =>(
                   </div>
              <img src='./assets/arrow.svg' alt='arrow' id='lbl-r'/>
       </div>
+      <div className='title-section'>
+        <div className='title-t'>Cult Unlimited Membership</div>
+        <div className='title-st'>Access to 100+ Cult centers | Free access to mind.fit and Cult outdoor centers | Attend unlimited number of classes</div>
+      </div>
+      <div className='all-packs'>
+      <a className='packs'>
+        <img className='pack-img' src = './assets/pack1.jpg' alt='p1'/>
+        <div className='price-orignal'>
+          <div className='p-det'>
+                Fitness pack at Cult
+          </div>
+          <div className='mrp'>
+                ₹11970
+            </div>
+        </div>
+        <div className='offer-price'>
+        <img className='price-img' src='./assets/price.svg' alt='ptag'/>
+        <div className='o-det'>
+             Offer Price:
+        </div>
+        <div className='price-val'>
+            ₹8991
+        </div>
+        </div>
+      </a>
+      <a className='packs'>
+        <img className='pack-img' src = './assets/pack2.jpg' alt='p2'/>
+        <div className='price-orignal'>
+          <div className='p-det'>
+                Fitness pack at Cult
+          </div>
+          <div className='mrp'>
+                ₹43380
+            </div>
+        </div>
+        <div className='offer-price'>
+        <img className='price-img' src='./assets/price.svg' alt='ptag'/>
+        <div className='o-det'>
+             Offer Price:
+        </div>
+        <div className='price-val'>
+            ₹22491
+        </div>
+        </div>
+        </a>
+      <a className='packs'>
+        <img className='pack-img' src = './assets/pack3.jpg' alt='p3'/>
+        <div className='price-orignal'>
+          <div className='p-det'>
+                Fitness pack at Cult
+          </div>
+          <div className='mrp'>
+                ₹22440
+            </div>
+        </div>
+        <div className='offer-price'>
+        <img className='price-img' src='./assets/price.svg' alt='ptag'/>
+        <div className='o-det'>
+             Offer Price:
+        </div>
+        <div className='price-val'>
+            ₹14391
+        </div>
+        </div>
+        </a>
+      <a className='packs'>
+        <img className='pack-img' src = './assets/pack4.jpg' alt='p4'/>
+        <div className='price-orignal'>
+          <div className='p-det'>
+                Fitness pack at Cult
+          </div>
+          <div className='mrp'>
+                ₹22440
+            </div>
+        </div>
+        <div className='offer-price'>
+        <img className='price-img' src='./assets/price.svg' alt='ptag'/>
+        <div className='o-det'>
+             Offer Price:
+        </div>
+        <div className='price-val'>
+            ₹15291
+        </div>
+        </div>
+      </a>
+      </div>
    </div>
 )
 
