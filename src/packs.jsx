a87a37430a8dbac624e1db52b7993e457755c3e0
Raj Sagar <therajsagar@gmail.com>
2019-03-03 16:34:32 +0530

    updated

--- src/packs.jsx
+++ src/packs.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import  './packs.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const Packs =() =>(
 <div>
@@ -112,18 +114,39 @@ const Packs =() =>(
       </div>
       <div className='workouts'>
           <div className='w-title'>Choose your workouts</div>
-      </div>
-      <div className='w-logo'>
-          <img src='./assets/w1.jpg' alt='w1'/>
-          <img src='./assets/w2.jpg' alt='w2'/>
-          <img src='./assets/w3.jpg' alt='w3'/>
-          <img src='./assets/w4.jpg' alt='w4'/>
-          <img src='./assets/w5.jpg' alt='w5'/>
-          <img src='./assets/w6.jpg' alt='w6'/>
-          <img src='./assets/w7.jpg' alt='w7'/>
-          <img src='./assets/w8.jpg' alt='w8'/>
-          <img src='./assets/w9.jpg' alt='w9'/>
-
+          <div className='w-logo'>
+              <img src='./assets/w1.jpg' alt='w1'/>
+              <img src='./assets/w2.jpg' alt='w2'/>
+              <img src='./assets/w3.jpg' alt='w3'/>
+              <img src='./assets/w4.jpg' alt='w4'/>
+              <img src='./assets/w5.jpg' alt='w5'/>
+              <img src='./assets/w6.jpg' alt='w6'/>
+              <img src='./assets/w7.jpg' alt='w7'/>
+              <img src='./assets/w8.jpg' alt='w8'/>
+              <img src='./assets/w9.jpg' alt='w9'/>
+          </div>
+          <div className='view'>
+          <div className='view-head'>
+          <div className='view-t'>Experience the space</div>
+          <div className='view-st'>State-of-the-art facility for all your fitness needs</div>
+          </div>
+          <div className='view-slider'>
+              <Carousel showArrows={false} showStatus={false} showThumbs={false} transitionTime={1000}>
+                <div>
+                     <img src='assets/v1.jpg' alt='v1' />
+                 </div>
+                 <div>
+                     <img src='assets/v2.jpg' alt='v2' />
+                 </div>
+                 <div>
+                     <img src='assets/v3.jpg' alt='v3' />
+                 </div>
+                 <div>
+                     <img src='assets/v4.jpg' alt='v4' />
+                 </div>
+              </Carousel>
+          </div>
+          </div>
       </div>
    </div>
 )
