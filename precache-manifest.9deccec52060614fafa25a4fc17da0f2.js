fd7ee6685b47f2a8dec29efdf5ca1b3fabd8ad0e
Raj Sagar <therajsagar@gmail.com>
2019-03-11 00:22:38 +0530

    Updates

---
+++ precache-manifest.9deccec52060614fafa25a4fc17da0f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "045ec67c3f7c0b37fefe",
+    "url": "/cultfit-UI/static/js/runtime~main.045ec67c.js"
+  },
+  {
+    "revision": "6dcb01cbf44068dda2b4",
+    "url": "/cultfit-UI/static/js/main.6dcb01cb.chunk.js"
+  },
+  {
+    "revision": "d917d4b8903acc3cdc20",
+    "url": "/cultfit-UI/static/js/2.d917d4b8.chunk.js"
+  },
+  {
+    "revision": "6dcb01cbf44068dda2b4",
+    "url": "/cultfit-UI/static/css/main.4aa2cafc.chunk.css"
+  },
+  {
+    "revision": "d917d4b8903acc3cdc20",
+    "url": "/cultfit-UI/static/css/2.a8fe1c89.chunk.css"
+  },
+  {
+    "revision": "fff60c2016d0087cdfa8ce876b68cb58",
+    "url": "/cultfit-UI/index.html"
+  }
+];
