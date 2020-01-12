09b41c9dda79d690b637fff7003e38035952e359
Raj Sagar <raj.sagar@bookmyshow.com>
2020-01-13 00:06:34 +0530

    Updates

---
+++ precache-manifest.cd6dd361d2fac6b24d9459de2901f126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82342ddec460b1384b7c0e62b037ce19",
+    "url": "/cultfit-UI/index.html"
+  },
+  {
+    "revision": "1b644c37da6d0036ea0a",
+    "url": "/cultfit-UI/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "b1b04b1d80a1d379541d",
+    "url": "/cultfit-UI/static/css/main.f958c2d4.chunk.css"
+  },
+  {
+    "revision": "1b644c37da6d0036ea0a",
+    "url": "/cultfit-UI/static/js/2.bf1523f5.chunk.js"
+  },
+  {
+    "revision": "b1b04b1d80a1d379541d",
+    "url": "/cultfit-UI/static/js/main.059231e0.chunk.js"
+  },
+  {
+    "revision": "4bf3df8191e93ed4dfe1",
+    "url": "/cultfit-UI/static/js/runtime-main.c1cfbe3a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cultfit-UI/precache-manifest.d5b620fde6d46ba18bd05c1fc2e9fe83.js"
+  "/cultfit-UI/precache-manifest.cd6dd361d2fac6b24d9459de2901f126.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cultfit-UI/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cultfit-UI/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
